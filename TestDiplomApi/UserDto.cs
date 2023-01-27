9b5c89db402402702695c661544e31e665809585
ilyas <ilyas@DESKTOP-2805>
2023-01-28 02:12:30 +0600

    Add project files.

---
+++ TestDiplomApi/UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace TestDiplomApi
+{
+    public class UserDto
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
